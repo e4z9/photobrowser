80b53896723e83e572df7594ff69f110a018eee4
Eike Ziller <git@eikeziller.de>
2024-03-16 10:58:12 +0100

    Add sodium-cxx as a submodule

--- cmake/FindSodiumCxx.cmake
+++ cmake/FindSodiumCxx.cmake
@@ -1,17 +1,24 @@
-find_package(Boost REQUIRED)
+find_package(Boost REQUIRED) # boost should be a public dependency of sodium-cxx instead
 
-find_library(SodiumCxx_LIBRARIES NAMES libsodium.a) # hmpf
-find_path(SodiumCxx_INCLUDE_DIRS NAMES sodium/sodium.h)
+if(TARGET libsodium)
+  # from submodule
+  add_library(SodiumCxx ALIAS libsodium)
+  set_target_properties(libsodium PROPERTIES
+                        INTERFACE_INCLUDE_DIRECTORIES "${sodium_BINARY_DIR}/include;${Boost_INCLUDE_DIR}")
+else()
+  find_library(SodiumCxx_LIBRARIES NAMES libsodium.a) # hmpf
+  find_path(SodiumCxx_INCLUDE_DIRS NAMES sodium/sodium.h)
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SodiumCxx DEFAULT_MSG SodiumCxx_LIBRARIES SodiumCxx_INCLUDE_DIRS)
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(SodiumCxx DEFAULT_MSG SodiumCxx_LIBRARIES SodiumCxx_INCLUDE_DIRS)
 
-if(SodiumCxx_FOUND AND NOT TARGET SodiumCxx)
-  add_library(SodiumCxx UNKNOWN IMPORTED)
-  set_target_properties(SodiumCxx PROPERTIES
-                        IMPORTED_LOCATION "${SodiumCxx_LIBRARIES}"
-                        INTERFACE_INCLUDE_DIRECTORIES "${SodiumCxx_INCLUDE_DIRS};${Boost_INCLUDE_DIR}"
-  )
+  if(SodiumCxx_FOUND AND NOT TARGET SodiumCxx)
+    add_library(SodiumCxx UNKNOWN IMPORTED)
+    set_target_properties(SodiumCxx PROPERTIES
+                          IMPORTED_LOCATION "${SodiumCxx_LIBRARIES}"
+                          INTERFACE_INCLUDE_DIRECTORIES "${SodiumCxx_INCLUDE_DIRS};${Boost_INCLUDE_DIR}"
+    )
+  endif()
 endif()
 
 include(FeatureSummary)
